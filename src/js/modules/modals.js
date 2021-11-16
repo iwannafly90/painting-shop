6cfe75d33963c67f454900bef4b23c38e2c09cfd
iwannafly90 <dima3429@mail.ru>
2021-11-16 21:45:58 +0300

    added modal windows

---
+++ src/js/modules/modals.js
@@ -0,0 +1,71 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]');
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault()
+                }
+
+                if (destroy) {
+                    item.remove();
+                }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+            });
+        });
+
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+        })
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+            }
+        })
+    }
+
+    function showModalByTime(selector, time) {
+        setTimeout(function () {
+            let display;
+
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    display = 'block';
+                }
+            });
+
+            if (!display) {
+                document.querySelector(selector).style.display = "block";
+                document.body.style.overflow = "hidden";
+            }
+        }, time)
+    }
+
+
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true)
+
+    showModalByTime('.popup-consultation', 5000);
+}
+
+export default modals;
