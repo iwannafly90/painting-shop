99352085f8c6a37f0afefd91e77f9999bd75f3e3
iwannafly90 <dima3429@mail.ru>
2021-11-18 12:27:23 +0300

    added sliders

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,4 +1,6 @@
 const modals = () => {
+    let btnPressed = false;
+
     function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
@@ -11,12 +13,15 @@ const modals = () => {
                     e.preventDefault()
                 }
 
+                btnPressed = true;
+
                 if (destroy) {
                     item.remove();
                 }
 
                 windows.forEach(item => {
                     item.style.display = 'none';
+                    item.classList.add('animated', 'fadeIn');
                 });
 
                 modal.style.display = "block";
@@ -61,10 +66,19 @@ const modals = () => {
         }, time)
     }
 
+    function openByScroll(selector) {
+        window.addEventListener('scroll', () => {
+            if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight)) {
+                document.querySelector(selector).click();
+            }
+        });
+    }
+
 
     bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
     bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
     bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true)
+    openByScroll(".fixed-gift");
 
     showModalByTime('.popup-consultation', 5000);
 }
